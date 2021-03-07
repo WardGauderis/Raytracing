c827e41e6a9132b504df30ac549166c4d0b3b097
Ward <ward.gauderis@student.uantwerpen.be>
2021-03-08 00:58:08 +0100

    image textures

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use std::f64::INFINITY;
-use std::fs::File;
-use std::io::Write;
-use std::rc::Rc;
+#![feature(destructuring_assignment)]
+use std::{f64::INFINITY, fs::File, io::Write, rc::Rc};
 
-use crate::bvh::BVHNode;
-use crate::camera::Camera;
-use crate::color::write_color;
-use crate::hittable::Hittable;
-use crate::hittable_list::HittableList;
-use crate::material::{Dielectric, Lambertian, Material, Metal};
-use crate::movingsphere::MovingSphere;
-use crate::ray::Ray;
-use crate::sphere::Sphere;
-use crate::texture::{CheckerTexture, NoiseTexture};
-use crate::util::{random_f64, random_f64_range};
-use crate::vec3::{unit_vector, Color, Point3, Vec3};
+use crate::{
+	bvh::BVHNode,
+	camera::Camera,
+	color::write_color,
+	hittable::Hittable,
+	hittable_list::HittableList,
+	material::{Dielectric, Lambertian, Material, Metal},
+	movingsphere::MovingSphere,
+	ray::Ray,
+	sphere::Sphere,
+	texture::{CheckerTexture, ImageTexture, NoiseTexture},
+	util::{random_f64, random_f64_range},
+	vec3::{unit_vector, Color, Point3, Vec3},
+};
 
 mod aabb;
 mod bvh;
@@ -31,38 +31,38 @@ mod texture;
 mod util;
 mod vec3;
 
-fn ray_color(r: &Ray, world: &dyn Hittable, depth: i32) -> Color {
+fn ray_color(r: &Ray, world: &dyn Hittable, depth: i32,) -> Color {
 	if depth <= 0 {
 		return Color::default();
 	}
 
-	if let Some(rec) = world.hit(r, 0.001, INFINITY) {
-		if let Some((attenuation, scattered)) = rec.mat_ptr.scatter(r, &rec) {
-			return attenuation * ray_color(&scattered, world, depth - 1);
+	if let Some(rec,) = world.hit(r, 0.001, INFINITY,) {
+		if let Some((attenuation, scattered,),) = rec.mat_ptr.scatter(r, &rec,) {
+			return attenuation * ray_color(&scattered, world, depth - 1,);
 		}
 		return Color::default();
 	}
-	let unit_direction = unit_vector(&r.direction());
+	let unit_direction = unit_vector(&r.direction(),);
 	let t = 0.5 * (unit_direction.y() + 1.0);
-	(1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+	(1.0 - t) * Color::new(1.0, 1.0, 1.0,) + t * Color::new(0.5, 0.7, 1.0,)
 }
 
 fn random_scene() -> HittableList {
 	let mut world = HittableList::default();
 
 	let checker = Rc::new(CheckerTexture::new(
-		Color::new(0.2, 0.3, 0.1),
-		Color::new(0.9, 0.9, 0.9),
-	));
-	let ground_material = Rc::new(Lambertian::from(checker));
+		Color::new(0.2, 0.3, 0.1,),
+		Color::new(0.9, 0.9, 0.9,),
+	),);
+	let ground_material = Rc::new(Lambertian::from(checker,),);
 	world.add(Rc::new(Sphere::new(
-		Point3::new(0.0, -1000.0, 0.0),
+		Point3::new(0.0, -1000.0, 0.0,),
 		1000.0,
 		ground_material,
-	)));
+	),),);
 
-	for a in -11..11 {
-		for b in -11..11 {
+	for a in -11 .. 11 {
+		for b in -11 .. 11 {
 			let choose_mat = random_f64();
 			let center = Point3::new(
 				a as f64 + 0.9 * random_f64(),
@@ -70,13 +70,13 @@ fn random_scene() -> HittableList {
 				b as f64 + 0.9 * random_f64(),
 			);
 
-			if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
-				let sphere_material: Rc<dyn Material>;
+			if (center - Point3::new(4.0, 0.2, 0.0,)).length() > 0.9 {
+				let sphere_material: Rc<dyn Material,>;
 
 				if choose_mat < 0.8 {
 					let albedo = Color::random() * Color::random();
-					sphere_material = Rc::new(Lambertian::new(albedo));
-					let center2 = center + Vec3::new(0.0, random_f64_range(0.0, 0.5), 0.0);
+					sphere_material = Rc::new(Lambertian::new(albedo,),);
+					let center2 = center + Vec3::new(0.0, random_f64_range(0.0, 0.5,), 0.0,);
 					world.add(Rc::new(MovingSphere::new(
 						center,
 						center2,
@@ -84,40 +84,40 @@ fn random_scene() -> HittableList {
 						1.0,
 						0.2,
 						sphere_material,
-					)));
+					),),);
 				} else if choose_mat < 0.95 {
-					let albedo = Color::random_in_range(0.5, 1.0);
-					let fuzz = random_f64_range(0.0, 0.5);
-					sphere_material = Rc::new(Metal::new(albedo, fuzz));
-					world.add(Rc::new(Sphere::new(center, 0.2, sphere_material)));
+					let albedo = Color::random_in_range(0.5, 1.0,);
+					let fuzz = random_f64_range(0.0, 0.5,);
+					sphere_material = Rc::new(Metal::new(albedo, fuzz,),);
+					world.add(Rc::new(Sphere::new(center, 0.2, sphere_material,),),);
 				} else {
-					sphere_material = Rc::new(Dielectric::new(1.5));
-					world.add(Rc::new(Sphere::new(center, 0.2, sphere_material)));
+					sphere_material = Rc::new(Dielectric::new(1.5,),);
+					world.add(Rc::new(Sphere::new(center, 0.2, sphere_material,),),);
 				}
 			}
 		}
 	}
 
-	let material1 = Rc::new(Dielectric::new(1.5));
+	let material1 = Rc::new(Dielectric::new(1.5,),);
 	world.add(Rc::new(Sphere::new(
-		Point3::new(0.0, 1.0, 0.0),
+		Point3::new(0.0, 1.0, 0.0,),
 		1.0,
 		material1,
-	)));
+	),),);
 
-	let material2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+	let material2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1,),),);
 	world.add(Rc::new(Sphere::new(
-		Point3::new(-4.0, 1.0, 0.0),
+		Point3::new(-4.0, 1.0, 0.0,),
 		1.0,
 		material2,
-	)));
+	),),);
 
-	let material3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+	let material3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5,), 0.0,),);
 	world.add(Rc::new(Sphere::new(
-		Point3::new(4.0, 1.0, 0.0),
+		Point3::new(4.0, 1.0, 0.0,),
 		1.0,
 		material3,
-	)));
+	),),);
 
 	world
 }
@@ -126,19 +126,19 @@ fn two_spheres() -> HittableList {
 	let mut objects = HittableList::default();
 
 	let checker = Rc::new(CheckerTexture::new(
-		Color::new(0.2, 0.3, 0.1),
-		Color::new(0.9, 0.9, 0.9),
-	));
+		Color::new(0.2, 0.3, 0.1,),
+		Color::new(0.9, 0.9, 0.9,),
+	),);
 	objects.add(Rc::new(Sphere::new(
-		Point3::new(0.0, -10.0, 0.0),
+		Point3::new(0.0, -10.0, 0.0,),
 		10.0,
-		Rc::new(Lambertian::from(checker.clone())),
-	)));
+		Rc::new(Lambertian::from(checker.clone(),),),
+	),),);
 	objects.add(Rc::new(Sphere::new(
-		Point3::new(0.0, 10.0, 0.0),
+		Point3::new(0.0, 10.0, 0.0,),
 		10.0,
-		Rc::new(Lambertian::from(checker)),
-	)));
+		Rc::new(Lambertian::from(checker,),),
+	),),);
 
 	objects
 }
@@ -146,19 +146,27 @@ fn two_spheres() -> HittableList {
 fn two_perlin_spheres() -> HittableList {
 	let mut objects = HittableList::default();
 
-	let pertext = Rc::new(NoiseTexture::new(4.0));
+	let pertext = Rc::new(NoiseTexture::new(4.0,),);
 	objects.add(Rc::new(Sphere::new(
-		Point3::new(0.0, -1000.0, 0.0),
+		Point3::new(0.0, -1000.0, 0.0,),
 		1000.0,
-		Rc::new(Lambertian::from(pertext.clone())),
-	)));
+		Rc::new(Lambertian::from(pertext.clone(),),),
+	),),);
 	objects.add(Rc::new(Sphere::new(
-		Point3::new(0.0, 2.0, 0.0),
+		Point3::new(0.0, 2.0, 0.0,),
 		2.0,
-		Rc::new(Lambertian::from(pertext)),
-	)));
+		Rc::new(Lambertian::from(pertext,),),
+	),),);
 
 	objects
+}
+
+fn earth() -> HittableList {
+	let earth_texture = Rc::new(ImageTexture::new("earthmap.jpg",),);
+	let earth_surface = Rc::new(Lambertian::from(earth_texture,),);
+	let globe = Rc::new(Sphere::new(Point3::default(), 2.0, earth_surface,),);
+
+	HittableList::new(globe,)
 }
 
 fn main() {
@@ -177,26 +185,32 @@ fn main() {
 	match 0 {
 		1 => {
 			world = random_scene();
-			lookfrom = Point3::new(13.0, 2.0, 3.0);
-			lookat = Point3::new(0.0, 0.0, 0.0);
+			lookfrom = Point3::new(13.0, 2.0, 3.0,);
+			lookat = Point3::new(0.0, 0.0, 0.0,);
 			vfov = 20.0;
 			aperture = 0.1;
-		}
+		},
 		2 => {
 			world = two_spheres();
-			lookfrom = Point3::new(13.0, 2.0, 3.0);
-			lookat = Point3::new(0.0, 0.0, 0.0);
+			lookfrom = Point3::new(13.0, 2.0, 3.0,);
+			lookat = Point3::new(0.0, 0.0, 0.0,);
 			vfov = 20.0;
-		}
-		_ => {
+		},
+		3 => {
 			world = two_perlin_spheres();
-			lookfrom = Point3::new(13.0, 2.0, 3.0);
-			lookat = Point3::new(0.0, 0.0, 0.0);
+			lookfrom = Point3::new(13.0, 2.0, 3.0,);
+			lookat = Point3::new(0.0, 0.0, 0.0,);
 			vfov = 20.0;
-		}
+		},
+		_ => {
+			world = earth();
+			lookfrom = Point3::new(13.0, 2.0, 3.0,);
+			lookat = Point3::default();
+			vfov = 20.0;
+		},
 	}
 
-	let vup = Vec3::new(0.0, 1.0, 0.0);
+	let vup = Vec3::new(0.0, 1.0, 0.0,);
 	let dist_to_focus = 10.0;
 
 	let cam = Camera::new(
@@ -211,21 +225,21 @@ fn main() {
 		1.0,
 	);
 
-	let mut file = File::create("test.ppm").unwrap();
+	let mut file = File::create("test.ppm",).unwrap();
 
 	write!(file, "P3\n{} {}\n255\n", image_width, image_height).unwrap();
 
-	for j in (0..image_height).into_iter().rev() {
+	for j in (0 .. image_height).into_iter().rev() {
 		eprintln!("\rScanlines remaining: {} ", j);
-		for i in 0..image_width {
+		for i in 0 .. image_width {
 			let mut pixel_color = Color::default();
-			for _ in 0..samples_per_pixel {
+			for _ in 0 .. samples_per_pixel {
 				let u = (i as f64 + random_f64()) / (image_width - 1) as f64;
 				let v = (j as f64 + random_f64()) / (image_height - 1) as f64;
-				let r = cam.get_ray(u, v);
-				pixel_color += ray_color(&r, &world, max_depth);
+				let r = cam.get_ray(u, v,);
+				pixel_color += ray_color(&r, &world, max_depth,);
 			}
-			write_color(&mut file, &pixel_color, samples_per_pixel);
+			write_color(&mut file, &pixel_color, samples_per_pixel,);
 		}
 	}
 	eprint!("\nDone\n");
--- src/texture.rs
+++ src/texture.rs
@@ -1,57 +1,63 @@
-use crate::perlin::Perlin;
-use crate::vec3::{Color, Point3, Vec3};
 use std::rc::Rc;
 
+use image::{flat::NormalForm::ImagePacked, open, GenericImageView};
+
+use crate::{
+	perlin::Perlin,
+	util::clamp,
+	vec3::{Color, Point3, Vec3},
+};
+
 pub trait Texture {
-	fn value(&self, u: f64, v: f64, p: &Point3) -> Color;
+	fn value(&self, u: f64, v: f64, p: &Point3,) -> Color;
 }
 
-#[derive(Default)]
+#[derive(Default,)]
 pub struct SolidColor {
 	color_value: Color,
 }
 
 impl SolidColor {
-	pub fn new(red: f64, green: f64, blue: f64) -> Self {
+	pub fn new(red: f64, green: f64, blue: f64,) -> Self {
 		SolidColor {
-			color_value: Color::new(red, green, blue),
+			color_value: Color::new(red, green, blue,),
 		}
 	}
 }
 
-impl From<Color> for SolidColor {
-	fn from(color: Color) -> Self {
-		SolidColor { color_value: color }
+impl From<Color,> for SolidColor {
+	fn from(color: Color,) -> Self {
+		SolidColor {
+			color_value: color,
+		}
 	}
 }
 
 impl Texture for SolidColor {
-	fn value(&self, u: f64, v: f64, p: &Point3) -> Color {
-		self.color_value
-	}
+	fn value(&self, u: f64, v: f64, p: &Point3,) -> Color { self.color_value }
 }
 
 pub struct CheckerTexture {
-	even: Rc<dyn Texture>,
-	odd: Rc<dyn Texture>,
+	even: Rc<dyn Texture,>,
+	odd:  Rc<dyn Texture,>,
 }
 
 impl CheckerTexture {
-	pub fn new(even: Color, odd: Color) -> Self {
+	pub fn new(even: Color, odd: Color,) -> Self {
 		CheckerTexture {
-			even: Rc::new(SolidColor::from(even)),
-			odd: Rc::new(SolidColor::from(odd)),
+			even: Rc::new(SolidColor::from(even,),),
+			odd:  Rc::new(SolidColor::from(odd,),),
 		}
 	}
 }
 
 impl Texture for CheckerTexture {
-	fn value(&self, u: f64, v: f64, p: &Point3) -> Color {
+	fn value(&self, u: f64, v: f64, p: &Point3,) -> Color {
 		let sines = (10.0 * p.x()).sin() * (10.0 * p.y()).sin() * (10.0 * p.z()).sin();
 		if sines < 0.0 {
-			self.odd.value(u, v, p)
+			self.odd.value(u, v, p,)
 		} else {
-			self.even.value(u, v, p)
+			self.even.value(u, v, p,)
 		}
 	}
 }
@@ -62,7 +68,7 @@ pub struct NoiseTexture {
 }
 
 impl NoiseTexture {
-	pub fn new(scale: f64) -> Self {
+	pub fn new(scale: f64,) -> Self {
 		NoiseTexture {
 			noise: Perlin::new(),
 			scale,
@@ -71,8 +77,74 @@ impl NoiseTexture {
 }
 
 impl Texture for NoiseTexture {
-	fn value(&self, u: f64, v: f64, p: &Vec3) -> Vec3 {
-		Color::new(1.0, 1.0, 1.0)
-			* 0.5 * (1.0 + (self.scale * p.z() + 10.0 * self.noise.turb(p, 7)).sin())
+	fn value(&self, u: f64, v: f64, p: &Vec3,) -> Vec3 {
+		Color::new(1.0, 1.0, 1.0,)
+			* 0.5 * (1.0 + (self.scale * p.z() + 10.0 * self.noise.turb(p, 7,)).sin())
+	}
+}
+
+pub struct ImageTexture {
+	data:               Option<Vec<u8,>,>,
+	width:              usize,
+	height:             usize,
+	bytes_per_scanline: usize,
+}
+
+impl ImageTexture {
+	const BYTES_PER_PIXEL: usize = 3;
+
+	pub fn new(name: &str,) -> Self {
+		let (mut width, mut height,) = (0, 0,);
+		let data = open(name,).map_or_else(
+			|e| {
+				eprintln!("{}", e.to_string());
+				None
+			},
+			|image| {
+				(width, height,) = image.dimensions();
+				Some(image.into_rgb8().into_raw(),)
+			},
+		);
+		let bytes_per_scanline = ImageTexture::BYTES_PER_PIXEL * width as usize;
+
+		ImageTexture {
+			data,
+			width: width as usize,
+			height: height as usize,
+			bytes_per_scanline,
+		}
+	}
+}
+
+impl Texture for ImageTexture {
+	fn value(&self, mut u: f64, mut v: f64, p: &Point3,) -> Color {
+		match &self.data {
+			None => Color::new(0.0, 1.0, 1.0,),
+			Some(data,) => {
+				u = clamp(u, 0.0, 1.0,);
+				// todo fix vertical mirror the proper way, maybe it's the image crate maybe it's me
+				v = 1.0 - clamp(v, 0.0, 1.0,);
+
+				let mut i = (u * self.width as f64) as usize;
+				let mut j = (v * self.height as f64) as usize;
+
+				if i >= self.width {
+					i = self.width - 1;
+				}
+				if j >= self.height {
+					j = self.height - 1;
+				}
+
+				let color_scale = 1.0 / 255.0;
+				let index = j * self.bytes_per_scanline + i * ImageTexture::BYTES_PER_PIXEL;
+				let pixel = &data[index .. index + 3];
+
+				Color::new(
+					color_scale * pixel[0] as f64,
+					color_scale * pixel[1] as f64,
+					color_scale * pixel[2] as f64,
+				)
+			},
+		}
 	}
 }
